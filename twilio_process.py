e559d4eb4432cf1763d5cef421ff55707d2b5e77
Jessica <jessica.dene.earley@gmail.com>
2015-11-19 11:45:31 -0800

    updated twilio with secret key info

--- twilio_process.py
+++ twilio_process.py
@@ -15,7 +15,7 @@ def send_text_message(phone):
 	    client = twilio.rest.TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 	 
 	    message = client.messages.create(
-	        body="I have your vehicle!",
+	        body="You are within 3 blocks of your destination, thank you for using Transit Alert",
 	        to=phone,
 	        from_=TWILIO_NUMBER
 	    )
