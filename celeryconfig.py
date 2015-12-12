0df74513cfa24bc07e7565c617832cb067b42ac5
Jessica <jessica.dene.earley@gmail.com>
2015-12-11 17:24:01 -0800

    edit celery for heroku

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,7 +5,7 @@ CELERY_IMPORTS = ('tasks')
 CELERY_IGNORE_RESULT = False
 BROKER_HOST = "127.0.0.1" #IP address of the server running RabbitMQ and Celery
 BROKER_PORT = 5672
-BROKER_URL='amqp://'
+BROKER_URL=os.environ.get('CLOUDAMQP_URL', 'amqp://')
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS=("tasks",)
 
