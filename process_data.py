44b202a2b2b2753909a12e060ba6105ec4d7a044
Jessica <jessica.dene.earley@gmail.com>
2016-02-04 13:41:52 -0800

    created a if for jsonResponse, need to research why google direction api isn't working on heroku

--- process_data.py
+++ process_data.py
@@ -261,7 +261,9 @@ def process_lat_lng_get_arrival_datetime(user_lat, user_lon, destination_lat, de
 
 	print "got the jsonResponse", jsonResponse
 
-	if jsonResponse == {u'geocoded_waypoints': [{u'geocoder_status': u'ZERO_RESULTS'}, {u'geocoder_status': u'ZERO_RESULTS'}], u'status': u'NOT_FOUND', u'routes': []}
+	empty_json = {u'geocoded_waypoints': [{u'geocoder_status': u'ZERO_RESULTS'}, {u'geocoder_status': u'ZERO_RESULTS'}], u'status': u'NOT_FOUND', u'routes': []}
+
+	if jsonResponse == empty_json:
 
 		arrival_time_raw =jsonResponse['routes'][0]['legs'][0]['arrival_time']['text']
 		arrival_time_raw_split = arrival_time_raw.split(":")
