d30daec4a0ddb78c075e1b498c7dbe5271402fd9
Jessica <jessica.dene.earley@gmail.com>
2016-02-25 15:49:10 -0800

    removed duplication lines

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,16 +1,14 @@
 # Configuration for Celery
 import os
+from celery.schedules import crontab
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_IGNORE_RESULT = False
-BROKER_HOST = "127.0.0.1" #IP address of the server running RabbitMQ and Celery
 BROKER_PORT = 5672
 BROKER_URL = os.environ.get('CLOUDAMQP_URL', 'amqp://')
-CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS = ("tasks",)
 CELERY_RESULT_BACKEND = None
 
-from celery.schedules import crontab
 
 CELERYBEAT_SCHEDULE = {
     'every-minute': {
